79723ccf21ccca336853e035c08fca78b2ed45c9
witek <witekdev@gmail.com>
2012-11-25 07:49:58 +0200

    add languages method to supply list of languages to template, this will likely be replaced in future via z3c form magic.

--- upfront/classlist/content/classlist.py
+++ upfront/classlist/content/classlist.py
@@ -3,6 +3,8 @@ from zope import schema
 from zope.interface import Interface
 from plone.directives import dexterity, form
 
+from upfront.classlist.vocabs import availableLanguages
+
 class IClassList(form.Schema):
     """ Description of ClassList content type
     """
@@ -17,12 +19,29 @@ class View(dexterity.DisplayForm):
     grok.template('viewclasslists')
     grok.require('zope2.View')
 
+    def languages(self):
+        """ Return the contents languages dictionary as a list of strings
+        """
+
+        language_vocab = availableLanguages(self.context).__iter__()
+        lang_list = []
+        notfinished = True;
+        while notfinished:            
+            try:
+                lang = language_vocab.next().title
+                lang_list.append(lang)
+            except StopIteration:
+                notfinished = False;
+
+        return lang_list
+
     def learners(self):
         """ Return all the learners in the current classlist folder
         """
         contentFilter = {
             'portal_type': 'upfront.classlist.content.learner'}
         return self.context.getFolderContents(contentFilter,full_objects=True)
+
 
 
 
