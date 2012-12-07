a474c9a7e0adf8f874db9462c6d1c8e867afff97
witek <witekdev@gmail.com>
2012-12-07 12:43:58 +0200

    fix typo.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '0.1'
 
 setup(name='upfront.classlist',
       version=version,
-      description="Package containing the TARMII dexterity content types",
+      description="Package containing the Upfront Classlist dexterity content types",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
